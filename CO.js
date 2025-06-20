7a7c80f840f124e37cf9cc4cde200c456f159139
RSJR-19 <sumbadrenan619@gmail.com>
2025-06-20 15:57:10 +0800

    account select CO

--- CO.js
+++ CO.js
@@ -1,11 +1,8 @@
 let amountValueCO = "";
 let chargeValueCO = "";
 let displayTotalCO = "";
-let rneeAccCO = document.getElementById("rneeCO");
-let lizaAccCO = document.getElementById("lizaCO");
 let checkerCO = 0; // if 0 wala if 1 rnee if 2 liza //
-let clearButton = document.getElementById("clearCO");
-clearButton.style.display = "none";
+
 
 
 
@@ -37,39 +34,34 @@ document.getElementById("totalCO").innerHTML= "Total: " + displayTotalCO;
 }
 }
 
+function validate(event) {
+    const clickedBox = event.target;
+    let rneeAccCO = document.getElementById("rneeCO");
+    let lizaAccCO = document.getElementById("lizaCO");
+    
 
-
-
-function clearCheck() {
-clearButton.style.display = "none";
-rneeAccCO.checked = false;
-lizaAccCO.checked = false;
-checkerCO = 0;
-
-
-validate()
-}
-
-function validate() {
-    if (rneeAccCO.checked || lizaAccCO.checked ){
-        if (rneeAccCO.checked) {
-            clearButton.style.display = "inline";
-            document.getElementById("lizaLabel").style.display = "none";
+    
+        if (clickedBox === rneeAccCO && rneeAccCO.checked) {
+            lizaAccCO.checked = false;
+            document.getElementById("rneeLabel").style.fontWeight = "bold";
+            document.getElementById("lizaLabel").style.fontWeight = "normal";
             checkerCO = 1;
+
         }
-        else if (lizaAccCO.checked) {
-             clearButton.style.display = "inline";
-            document.getElementById("rneeLabel").style.display = "none";
+        else if (clickedBox === lizaAccCO && lizaAccCO.checked) {
+            rneeAccCO.checked = false;
+            document.getElementById("lizaLabel").style.fontWeight= "bold";
+            document.getElementById("rneeLabel").style.fontWeight= "normal";
             checkerCO = 2;
         
-        }}
+        }
     else {
-        document.getElementById("rneeLabel").style.display = "inline";
-        document.getElementById("lizaLabel").style.display = "inline";
-        clearButton.style.display = "none";
+        document.getElementById("rneeLabel").style.fontWeight = "normal";
+        document.getElementById("lizaLabel").style.fontWeight = "normal";
         checkerCO = 0;
-    }}
-
+       
+    }
+}
 
     
 
