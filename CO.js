2a58fc374fbd212c0cc33d6c994353db2e4c5703
RSJR-19 <sumbadrenan619@gmail.com>
2025-06-23 14:14:40 +0800

    magic numbers gone

--- CO.js
+++ CO.js
@@ -1,147 +1,158 @@
 let amountValueCO = "0";
 let chargeValueCO = "";
 let displayTotalCO = "";
-let checkerCO = 0; // if 0 wala if 1 rnee if 2 liza //
+let checkerCO = "void"; // if void wala  //
 document.getElementById("totalCO").style.textAlign = "justify";
 
-
-
-
-function amountCO () {
-amountValueCO = Number(document.getElementById("amountInputCO").value);
-let transacChargeCO = Math.ceil(amountValueCO /500)*5;
-document.getElementById("amountDisplay").innerHTML = "Amount: " + "Php " + amountValueCO + ".00" + " ( " + transacChargeCO + " Charge)";
-document.getElementById("chargeInputCO").focus();
-computeTotalCO();
-confirmCO ();
-document.getElementById("totalCO").style.textAlign = "justify";
+function amountCO() {
+  amountValueCO = Number(document.getElementById("amountInputCO").value);
+  let transacChargeCO = Math.ceil(amountValueCO / 500) * 5;
+  document.getElementById("amountDisplay").innerHTML =
+    "Amount: " +
+    "Php " +
+    amountValueCO +
+    ".00" +
+    " ( " +
+    transacChargeCO +
+    " Charge)";
+  document.getElementById("chargeInputCO").focus();
+  computeTotalCO();
+  confirmCO();
+  document.getElementById("totalCO").style.textAlign = "justify";
 }
 
-document.getElementById("amountInputCO").addEventListener("keydown", function(event)  {
-if (event.key === "Enter" ) { amountCO(); 
-} 
-});
+document
+  .getElementById("amountInputCO")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      amountCO();
+    }
+  });
 
-function chargeCO () {
-chargeValueCO = Number(document.getElementById("chargeInputCO").value);
-computeTotalCO();
+function chargeCO() {
+  chargeValueCO = Number(document.getElementById("chargeInputCO").value);
+  computeTotalCO();
 }
 
-document.getElementById("chargeInputCO").addEventListener("keydown", function (event) {
-if (event.key === "Enter" ) { chargeCO();
-}
-});
+document
+  .getElementById("chargeInputCO")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      chargeCO();
+    }
+  });
 
 function computeTotalCO() {
-
-transacChargeCO = Math.ceil(amountValueCO /500)*5;
-if (amountValueCO > 0 && chargeValueCO >= 0){
-if (chargeValueCO > transacChargeCO){
-displayTotalCO = amountValueCO + (chargeValueCO - transacChargeCO );
-document.getElementById("totalCO").innerHTML= "Total: " + "Php " + displayTotalCO + ".00";
+  transacChargeCO = Math.ceil(amountValueCO / 500) * 5;
+  if (amountValueCO > 0 && chargeValueCO >= 0) {
+    if (chargeValueCO > transacChargeCO) {
+      displayTotalCO = amountValueCO + (chargeValueCO - transacChargeCO);
+      document.getElementById("totalCO").innerHTML =
+        "Total: " + "Php " + displayTotalCO + ".00";
+    } else {
+      displayTotalCO =
+        amountValueCO - Math.abs(transacChargeCO - chargeValueCO);
+      document.getElementById("totalCO").innerHTML =
+        "Total: " + "Php " + displayTotalCO + ".00";
+    }
+  } else if (amountValueCO === 0) {
+    document.getElementById("amountDisplay").innerHTML =
+      "Amount: Input valid Amount * ";
+    document.getElementById("totalCO").innerHTML = "Total: ";
+  }
 }
-else {
-displayTotalCO = amountValueCO - Math.abs(transacChargeCO - chargeValueCO );
-document.getElementById("totalCO").innerHTML= "Total: " + "Php " + displayTotalCO + ".00";
-}}
-else if (amountValueCO === 0){
-document.getElementById("amountDisplay").innerHTML= "Amount: Input valid Amount * ";
-document.getElementById("totalCO").innerHTML= "Total: ";
-
-
-}}
-
 
 function validate(event) {
-    const clickedBox = event.target;
-    let rneeAccCO = document.getElementById("rneeCO");
-    let lizaAccCO = document.getElementById("lizaCO");
-    
+  const clickedBox = event.target;
+  let rneeAccCO = document.getElementById("rneeCO");
+  let lizaAccCO = document.getElementById("lizaCO");
 
-    
-        if (clickedBox === rneeAccCO && rneeAccCO.checked) {
-            lizaAccCO.checked = false;
-            document.getElementById("rneeLabel").style.fontWeight = "bold";
-            document.getElementById("lizaLabel").style.fontWeight = "normal";
-            checkerCO = 1;
-            confirmCO();
-            amountCO();
-
-        }
-        else if (clickedBox === lizaAccCO && lizaAccCO.checked) {
-            rneeAccCO.checked = false;
-            document.getElementById("lizaLabel").style.fontWeight= "bold";
-            document.getElementById("rneeLabel").style.fontWeight= "normal";
-            checkerCO = 2;
-            confirmCO();
-            amountCO();
-        
-        }
-    else {
-        document.getElementById("rneeLabel").style.fontWeight = "normal";
-        document.getElementById("lizaLabel").style.fontWeight = "normal";
-        checkerCO = 0;
-        confirmCO();
-       
-    }
+  if (clickedBox === rneeAccCO && rneeAccCO.checked) {
+    lizaAccCO.checked = false;
+    document.getElementById("rneeLabel").style.fontWeight = "bold";
+    document.getElementById("lizaLabel").style.fontWeight = "normal";
+    checkerCO = "rnee";
+    confirmCO();
+    amountCO();
+  } else if (clickedBox === lizaAccCO && lizaAccCO.checked) {
+    rneeAccCO.checked = false;
+    document.getElementById("lizaLabel").style.fontWeight = "bold";
+    document.getElementById("rneeLabel").style.fontWeight = "normal";
+    checkerCO = "liza";
+    confirmCO();
+    amountCO();
+  } else {
+    document.getElementById("rneeLabel").style.fontWeight = "normal";
+    document.getElementById("lizaLabel").style.fontWeight = "normal";
+    checkerCO = "void";
+    confirmCO();
+  }
 }
-
 
 //confirm details final//
 
 function confirmCO() {
-    if ((checkerCO === 1 || checkerCO === 2) && (amountValueCO - transacChargeCO > 0)){
-        document.getElementById("confirmDetailsCO").style.backgroundColor = "yellow";
-    }
-    else if (amountValueCO - transacChargeCO <= 0 ) {
-        document.getElementById("confirmDetailsCO").style.backgroundColor = "gray";
-    }
-    else {
-        document.getElementById("confirmDetailsCO").style.backgroundColor = "gray";
-        checkerCO = 0;
-    }}
-
-    
-function confirmTransacCO() {
-    if (document.getElementById("confirmDetailsCO").style.backgroundColor === "yellow") {
-       const displayTimeCO = new Date().toLocaleTimeString();
-       const accountUsed = (checkerCO === 1 ? " R ": (checkerCO === 2 ? " L " : " " ));
-        document.getElementById("overlayCO").style.display = "flex";
-        if (chargeValueCO > 0) {
-    document.getElementById("detailsCO").innerHTML = "CO:" + accountUsed + amountValueCO + " = " + displayTotalCO + "<br>(Nagbigay " + chargeValueCO + ")";
-    document.getElementById("dateCO").innerHTML = "(" + displayTimeCO.toLocaleString() + ")";
-    let logDetailsCO = document.getElementById("detailsCO").innerText;
-    let logDateCO = document.getElementById("dateCO").innerText;
-    let totalLogCO = logDetailsCO + " " + logDateCO;
-
-    let existingLogs = JSON.parse(localStorage.getItem("transactionLogs")) || [];
-    existingLogs.push(totalLogCO);
-    localStorage.setItem("transactionLogs",JSON.stringify(existingLogs));
-    }
-    else{
-    document.getElementById("detailsCO").innerText = "CO:" + accountUsed + amountValueCO + " = " + displayTotalCO;
-    document.getElementById("dateCO").innerHTML = "(" + displayTimeCO.toLocaleString() + ")";
-    let logDetailsCO = document.getElementById("detailsCO").innerText;
-    let logDateCO = document.getElementById("dateCO").innerText;
-    let totalLogCO = logDetailsCO + " " + logDateCO;
-
-    let existingLogs = JSON.parse(localStorage.getItem("transactionLogs")) || [];
-    existingLogs.push(totalLogCO);
-    localStorage.setItem("transactionLogs",JSON.stringify(existingLogs));
-    }
-    }
-    else {
-        document.getElementById("totalCO").innerHTML = "Make sure the transaction is valid before confirming.";
-        document.getElementById("totalCO").style.textAlign = "center";
-    }
-
-
+  if (
+    (checkerCO === "rnee" || checkerCO === "liza") &&
+    amountValueCO - transacChargeCO > 0
+  ) {
+    document.getElementById("confirmDetailsCO").style.backgroundColor =
+      "yellow";
+  } else if (amountValueCO - transacChargeCO <= 0) {
+    document.getElementById("confirmDetailsCO").style.backgroundColor = "gray";
+  } else {
+    document.getElementById("confirmDetailsCO").style.backgroundColor = "gray";
+    checkerCO = "void";
+  }
 }
 
+function confirmTransacCO() {
+  if (
+    document.getElementById("confirmDetailsCO").style.backgroundColor ===
+    "yellow"
+  ) {
+    const displayTimeCO = new Date().toLocaleTimeString();
+    const accountUsed = checkerCO === "rnee" ? " R " : checkerCO === "liza" ? " L " : " ";
+    document.getElementById("overlayCO").style.display = "flex";
+    if (chargeValueCO > 0) {
+      document.getElementById("detailsCO").innerHTML =
+        "CO:" +
+        accountUsed +
+        amountValueCO +
+        " = " +
+        displayTotalCO +
+        "<br>(Nagbigay " +
+        chargeValueCO +
+        ")";
+      document.getElementById("dateCO").innerHTML =
+        "(" + displayTimeCO.toLocaleString() + ")";
+      let logDetailsCO = document.getElementById("detailsCO").innerText;
+      let logDateCO = document.getElementById("dateCO").innerText;
+      let totalLogCO = logDetailsCO + " " + logDateCO;
+
+      let existingLogs =
+        JSON.parse(localStorage.getItem("transactionLogs")) || [];
+      existingLogs.push(totalLogCO);
+      localStorage.setItem("transactionLogs", JSON.stringify(existingLogs));
+    } else {
+      document.getElementById("detailsCO").innerText =
+        "CO:" + accountUsed + amountValueCO + " = " + displayTotalCO;
+      document.getElementById("dateCO").innerHTML =
+        "(" + displayTimeCO.toLocaleString() + ")";
+      let logDetailsCO = document.getElementById("detailsCO").innerText;
+      let logDateCO = document.getElementById("dateCO").innerText;
+      let totalLogCO = logDetailsCO + " " + logDateCO;
+
+      let existingLogs =
+        JSON.parse(localStorage.getItem("transactionLogs")) || [];
+      existingLogs.push(totalLogCO);
+      localStorage.setItem("transactionLogs", JSON.stringify(existingLogs));
+    }
+  } else {
+    document.getElementById("totalCO").innerHTML =
+      "Make sure the transaction is valid before confirming.";
+    document.getElementById("totalCO").style.textAlign = "center";
+  }
+}
 
 document.getElementById("detailsCO").style.textAlign = "center";
-
-
-
-
-
--- cashIn.js
+++ cashIn.js
@@ -1,109 +1,97 @@
 let amountCI = 0;
 let paymentCI = 0;
-let checker = 0;
+let checker = false;
 let charge = 0;
 let toPay = 0;
 let changeCI = " ";
 
-
-
 function amount() {
- amountCI = parseFloat(document.getElementById("amountSend").value);
- charge = Math.ceil(amountCI/ 500)*5;
- document.getElementById("amountCI").innerHTML="Amount paid: " + "Php " + amountCI + ".00 " +"( " + charge + " Charge)";
- document.getElementById("paymentRcvd").focus();
- 
+  amountCI = parseFloat(document.getElementById("amountSend").value);
+  charge = Math.ceil(amountCI / 500) * 5;
+  document.getElementById("amountCI").innerHTML =
+    "Amount paid: " + "Php " + amountCI + ".00 " + "( " + charge + " Charge)";
+  document.getElementById("paymentRcvd").focus();
 }
 
-document.getElementById("amountSend").addEventListener("keydown",function (event) {
-    if (event.key === "Enter") { amount();compute();
+document
+  .getElementById("amountSend")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      amount();
+      compute();
     }
-});
+  });
 
 function payment() {
-paymentCI = parseFloat(document.getElementById("paymentRcvd").value);
-document.getElementById("paymentCI").innerHTML="Payment given: " + "Php " + paymentCI + ".00";
-
+  paymentCI = parseFloat(document.getElementById("paymentRcvd").value);
+  document.getElementById("paymentCI").innerHTML =
+    "Payment given: " + "Php " + paymentCI + ".00";
 }
 
-document.getElementById("paymentRcvd").addEventListener("keydown",function (event) {
-    if (event.key === "Enter") { payment(); compute();
+document
+  .getElementById("paymentRcvd")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      payment();
+      compute();
     }
-});
+  });
 
+function compute() {
+  charge = Math.ceil(amountCI / 500) * 5;
+  toPay = amountCI + charge;
 
+  if (amountCI > 0 && paymentCI > 0) {
+    checker = true;
+    if (amountCI === paymentCI) {
+      changeCI = "Walang bayad charge?";
+      document.getElementById("confirmCI").style.backgroundColor = "gray";
+      checker = false;
+    } else if (toPay > paymentCI) {
+      changeCI = "TANGAY PATI PUHUNAN";
+      document.getElementById("confirmCI").style.backgroundColor = "gray";
+      checker = false;
+    } else {
+      changeCI = "Change: " + "Php " + (paymentCI - toPay) + ".00";
+      document.getElementById("confirmCI").style.backgroundColor = "yellow";
+      checker = true;
+      document.getElementById("changeCI").style.textAlign = "justify";
+    }
 
-function compute () {
-charge = Math.ceil(amountCI / 500) *5;
-toPay = amountCI + charge;
-
-if (amountCI > 0 && paymentCI > 0) {
-     checker = 1;
-     if (amountCI === paymentCI) {
-     changeCI = "Walang bayad charge?";
-        document.getElementById("confirmCI").style.backgroundColor = "gray";
-        checker = 0;
-}
-
-else if (toPay > paymentCI) {
-     changeCI = "TANGAY PATI PUHUNAN"; 
-        document.getElementById("confirmCI").style.backgroundColor = "gray";
-        checker = 0;
-}
-
-else{
-    changeCI = "Change: " + "Php " + (paymentCI - toPay) + ".00";
-       document.getElementById("confirmCI").style.backgroundColor = "yellow"
-       checker = 1;
-       document.getElementById("changeCI").style.textAlign = "justify"; 
-}
-
- ;
-
- document.getElementById("changeCI").innerHTML = changeCI;
+    document.getElementById("changeCI").innerHTML = changeCI;
 
     return changeCI;
-
+  } else {
+    checker = false;
+    document.getElementById("confirmCI").style.backgroundColor = "gray";
+  }
 }
 
+function confirm() {
+  if (checker === true) {
+    let displayTime = new Date().toLocaleTimeString();
+    document.getElementById("overlayCI").style.display = "flex";
+    document.getElementById("detailsCI").innerHTML =
+      "CI: " + amountCI + " / " + paymentCI + " = " + (paymentCI - toPay);
+    document.getElementById("dateCI").innerHTML =
+      "(" + displayTime.toLocaleString() + ")";
+    let logDetailsCI = document.getElementById("detailsCI").innerText;
+    let logDateCI = document.getElementById("dateCI").innerText;
+    let totalLogCI = logDetailsCI + " " + logDateCI;
 
-
-
-else {
-checker = 0;
-document.getElementById("confirmCI").style.backgroundColor = "gray";
-}
-
-
-
-}
-
- function confirm() {
-if (checker === 1){
-let displayTime = new Date().toLocaleTimeString();
-document.getElementById("overlayCI").style.display="flex";
-document.getElementById("detailsCI").innerHTML= "CI: " + amountCI + " / " + paymentCI + " = " + (paymentCI - toPay);
-document.getElementById("dateCI").innerHTML= "(" + displayTime.toLocaleString() + ")";
-let logDetailsCI = document.getElementById("detailsCI").innerText;
-let logDateCI = document.getElementById("dateCI").innerText;
-let totalLogCI = logDetailsCI + " " +  logDateCI;
-
-let existingLogs = JSON.parse(localStorage.getItem("transactionLogs")) || [];
-/* "localStorage.getItem("transactionLogs")" bali tinitignan muna kung merong existing na na array para sa logs
+    let existingLogs =
+      JSON.parse(localStorage.getItem("transactionLogs")) || [];
+    /* "localStorage.getItem("transactionLogs")" bali tinitignan muna kung merong existing na na array para sa logs
 JSON.parse = para syang parse.float kung san ung string ibabalik niya to object form 
 JSON.stringify = pang convert ng mga object sa string para pwede sila sa localStorage
  || [] = para siyang "else" na kapag ka wala pang value or wala pang existing array, gagawa or gamit nalang ng new array
  */
 
-existingLogs.push(totalLogCI); //yung latest na value ng totalLogCI "pinupush" or dinadagdag sa array //
-localStorage.setItem("transactionLogs",JSON.stringify(existingLogs)); // sinesave ung details sa browser //
-
-}
-
-else {
+    existingLogs.push(totalLogCI); //yung latest na value ng totalLogCI "pinupush" or dinadagdag sa array //
+    localStorage.setItem("transactionLogs", JSON.stringify(existingLogs)); // sinesave ung details sa browser //
+  } else {
     document.getElementById("changeCI").style.textAlign = "center";
-    document.getElementById("changeCI").innerHTML = "Make sure the transaction is valid before confirming.";
+    document.getElementById("changeCI").innerHTML =
+      "Make sure the transaction is valid before confirming.";
+  }
 }
- }
-
-
