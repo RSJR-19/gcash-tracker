296de28032b4fed84105b90bf467cfb3e8ee5a69
RSJR-19 <sumbadrenan619@gmail.com>
2025-06-25 22:18:01 +0800

    working confirm Payment

--- Payment.js
+++ Payment.js
@@ -1,23 +1,49 @@
-let paymentAmountValue = document.getElementById("amountP");
+let paymentAmount = document.getElementById("amountP");
 let paymentPurpose = document.getElementById("purposeP");
+let displayPaidAmount = document.getElementById("paid-amount");
+let displayTotal = document.getElementById("totalP");
+let getConfirmButton = document.getElementById("confirm-P");
+let getOverlayPayment = document.getElementById("overlay-P");
 let isTherePayment = false;
 let isTherePurpose = false;
+displayTotal.style.textAlign = "justify";
+
+
+  function isAmountAndPurposeTrue() {
+if(isTherePayment === true && isTherePurpose === true){
+  let getPaymentAmount = paymentAmount.value;
+  let getPurposeDetail = paymentPurpose.value;
+  displayTotal.style.textAlign = "justify";
+  displayTotal.innerHTML = `Total: ${getPaymentAmount} (${getPurposeDetail})`;
+  getConfirmButton.style.backgroundColor = "yellow";
+  }
+  else{
+  displayTotal.style.textAlign = "justify";
+  displayTotal.innerHTML = "Total: ";
+  getConfirmButton.style.backgroundColor = "gray";
+  }
+}
 
 function paymentEnter() {
-  paymentAmountValue.value;
-  document.getElementById("paid-amount").innerHTML =
-    "Paid amount: " + "Php " + paymentAmountValue.value + ".00";
-  if (isTherePurpose === true) {
-    //once the confirm details is now ready //
-  } else {
-    document.getElementById("purposeP").focus();
-  }
-  if (paymentAmountValue.value > 0){
-    isTherePayment = true;
-  }
+  let getPaymentAmount = parseFloat(paymentAmount.value);
+  if (getPaymentAmount > 0 && getPaymentAmount !== ""){
+  displayPaidAmount.innerHTML =  `Paid amount: Php ${getPaymentAmount.toFixed(2)}`;
+  displayPaidAmount.style.color = "black";
+  displayTotal.innerHTML = "Total: "
+  displayTotal.style.textAlign = "justify";
+  console.log(displayPaidAmount);
+  isTherePayment = true;
+  if(isTherePurpose === true ? isAmountAndPurposeTrue(): paymentPurpose.focus());
+}
+
   else {
+    displayPaidAmount.innerHTML = "Paid amount: Invalid input *";
+    displayPaidAmount.style.color = "red";
     isTherePayment = false;
+    isAmountAndPurposeTrue();
   }
+  
+
 }
 
 document
@@ -28,23 +54,29 @@ document
     }
   });
 
+
+
 function purposeEnter() {
-  paymentPurpose.value;
-  document.getElementById("totalP").innerHTML =
-    "Total: " + paymentPurpose.value;
-    if (isTherePayment === false){
-        document.getElementById("amountP").focus();
-    }
-    else {
-        // switch to confirm button soon//
-    }
-    if (paymentPurpose.value.trim() !== ""){
-        isTherePurpose = true;
-    }
-    else {
-        isTherePurpose = false;
-    }
-}
+  let getPurposeDetail = paymentPurpose.value;
+  if(getPurposeDetail !== ""){
+    document.getElementById("purpose-status").innerHTML = "Purpose: ";
+    document.getElementById("purpose-status").style.color = "red";
+    document.getElementById("purpose-status").style.color = "black";
+    let getPaymentAmount = parseFloat(paymentAmount.value);
+    isTherePurpose = true;
+    if(isTherePayment === true ? isAmountAndPurposeTrue(): paymentAmount.focus());
+  }
+  else {
+    document.getElementById("purpose-status").innerHTML = "Please Input valid purpose *";
+    document.getElementById("purpose-status").style.color = "red";
+    isTherePurpose = false;
+    isAmountAndPurposeTrue();
+  }
+  
+  
+
+
+}//do later if isTherePayment == false //
 
 document
   .getElementById("purposeP")
@@ -53,3 +85,26 @@ document
       purposeEnter();
     }
   });
+
+  function overlayP () {
+if(getConfirmButton.style.backgroundColor ==="yellow"){
+   getOverlayPayment.style.display = "flex" 
+ }
+ else{
+   displayTotal.innerHTML = "Make sure both inputs are filled in properly."
+   displayTotal.style.textAlign = "center";
+   displayTotal.style.color = "red";
+   if(isTherePayment === false){
+     paymentAmount.focus();
+   }
+   else {
+    paymentEnter.focus()
+   }
+  }}
+
+
+
+
+
+
+  
